5643c769d74eed73320cc9a8801405994d904579
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-06 14:02:35 -0600

    added welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
-import "./App.css";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { Welcome } from "./Welcome";
 
 function App() {
   return (
     <div>
+      <Welcome />
       <Header />
       <Content />
       <Footer />
---
+++ src/Welcome.jsx
@@ -0,0 +1,8 @@
+export function Welcome() {
+  return (
+    <div id="welcome">
+      <h1>Something</h1>
+      <p>Bakery and Pattiserie</p>
+    </div>
+  );
+}
